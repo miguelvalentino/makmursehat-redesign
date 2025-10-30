f1d2c06bc070f6e4083fce49c43637c82889198b
miguel valentino <miguelvalentiono@gmail.com>
2025-10-30 19:50:33 +0700

    edit navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import searchIcon from '../assets/search.png'
 
 function Navbar() {
   return (
@@ -7,6 +8,12 @@ function Navbar() {
         <nav>
             <div className='logo'>
                 <h2>MakmurSehat</h2>
+            </div>
+            <div className='search-bar'>
+                <input type="text" placeholder="Cari..." />
+                <button type="submit">
+                    <img src={searchIcon} alt="Cari" />
+                </button>
             </div>
             <ul className='nav-link'>
                 <li><a href="/home">Home</a></li>
