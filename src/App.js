6cbcf0dc90d6e602b7d2da1a9412ae55f54be221
miguel valentino <miguelvalentiono@gmail.com>
2025-12-31 10:48:50 +0700

    multiple page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react"
 import Navbar from "./components/Navbar"
-
+import Carousel from "./components/carousel";
 function App() {
-  return <Navbar />
+  return (
+    <div>
+      <Navbar />
+      <Carousel />
+    </div>
+  );
 }
 
 export default App
---
+++ src/components/HomeSection.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Slider from ".../inc/Slider";
+
+function Home(){
+    return(
+        <div>
+           <Slider/>
+
+           <section className="section">
+
+           </section>
+
+
+        </div>
+    )
+}
